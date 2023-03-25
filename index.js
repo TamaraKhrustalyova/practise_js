43d3ae3922a42a68ade68a4032314a4cdd87def9
TamaraKhrustalyova <tamara.khrustalyova@gmail.com>
2023-03-25 20:05:47 +0200

    gg

---
+++ index.js
@@ -0,0 +1,25 @@
+const x = 10.4;
+const y = 5;
+
+// Додавання
+console.log(x + y);
+
+// // Віднімання
+// console.log(x - y); // 5
+
+// // Множення
+// console.log(x * y); // 50
+
+// // Ділення
+// console.log(x / y); // 2
+
+console.log (x % y);
+
+const pricePerItem = 3500;
+const orderedQuantity = 4;
+
+const totalPrice = pricePerItem;
+
+console.log(totalPrice * orderedQuantity)
+
+
